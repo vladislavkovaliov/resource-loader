cc09fe99df3f23ed50973e3cf2d2ac9e31fc492b
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2026-06-16 18:01:03 +0300

    feat: updates semantic release conf

--- release.config.js
+++ release.config.js
@@ -45,7 +45,7 @@ module.exports = {
         [
             '@semantic-release/npm',
             {
-                npmPublish: false,
+                npmPublish: true,
             }
         ],
         ['@semantic-release/git', {
